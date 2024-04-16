c1939ece404abd45b555dbad794701108b3bc3ec
nOneOrZero <snigurowicz.marek@gmail.com>
2024-04-16 04:50:08 +0200

    Dodałem rejestrowanie każdego zdażenia pracownika. Każda operacja jest zapisana w odpowiedniej tabeli z aktualnym czasem

--- work/changeValuePoint.php
+++ work/changeValuePoint.php
@@ -20,6 +20,11 @@
 
       $idButt = $_POST['idButt'];
 
+      $time = date("H:i");
+      $dateNow = date("Y-m-d");
+
+      $addLog=$db->prepare('INSERT INTO `history_one_day` (`id`, `id_user`, `id_place`, `operation`, `note`, `note_url`, `time`, `date`) VALUES (NULL, :idUser, :idPlace, :operation, "", "", :time, :date); ');
+
       if($idButt=="sun_SP"){
         $update=$db->prepare('UPDATE notepad SET sunbed=sunbed+1 WHERE id_place=:idPlace AND state_sun>sunbed');
       }else if($idButt=="sun_SM"){
@@ -52,6 +57,25 @@
       $update->bindValue(':idPlace', $_SESSION['idPlace'], PDO::PARAM_INT);
       
       if($update->execute()){
+
+        $addLog->bindValue(':idUser', $_SESSION['logged']['id'], PDO::PARAM_INT);
+        $addLog->bindValue(':idPlace', $_SESSION['idPlace'], PDO::PARAM_INT);
+        $addLog->bindValue(':operation', $idButt, PDO::PARAM_STR);
+        $addLog->bindValue(':time', $time, PDO::PARAM_STR);
+        $addLog->bindValue(':date', $dateNow, PDO::PARAM_STR);
+
+        if($addLog->execute()){
+
+          if($addLog->rowCount()>0){
+            require_once 'masterPoint.php';
+            exit();
+          }else{
+
+          }
+          
+        }else{
+
+        }
 
         require_once 'masterPoint.php';
         exit();
